b9f8e8e80f1cd784da9dbe48c2149c0c362bfbb9
SaraVanan S <112925088+Saravanan-1213@users.noreply.github.com>
2022-12-28 20:07:21 +0530

    Add files via upload
    Uploaded Task roadmap 35

---
+++ Answer.sql
@@ -0,0 +1,23 @@
+-- // 1 => Movie should have multiple media(Video or Image)
+
+Select * from Movie 
+
+-- 2 = > Movie can belongs to multiple Genre 
+
+Select title FROM movies
+WHERE  Horrer and Family Entertainer 
+
+-- 3 => Movie can have multiple reviews and Review can belongs to a use
+
+Select Title From Movies 
+Where reviews as GoodandBad
+
+-- 4 => Artist can have multiple skills
+
+Selet Title from movies Where Skills
+
+
+-- 5 => Artist can perform multiple role in a single film
+
+Select Title from Moives 
+where Actor as Multiple Role
